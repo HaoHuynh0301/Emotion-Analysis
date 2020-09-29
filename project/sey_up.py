50456e13b9f009bc88361dcccd40218d46f89a82
HaoHuynh <hao152903@gmail.com>
2020-09-29 10:17:18 +0700

    29/09/2020 - Update 02

--- project/sey_up.py
+++ project/sey_up.py
@@ -2,5 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import tensorflow as tf
+from keras.utils import np_utils
+from keras.regularizers import l2
+import sys
+import os
 import cv2
 import keras
+
+data=pd.read_csv('fer2013.csv')
+# print(data["Usage"].value_counts())
+# print(data)
+
+x_train, y_train, x_test, y_test=[],[],[],[]
+
